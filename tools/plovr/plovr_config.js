f9205029ae495e46b71f0279173392beb92a60c3
niclas norgren <niclas@bindows.net>
2013-03-10 17:16:46 +0100

    adjusting path to init file

--- tools/plovr/plovr_config.js
+++ tools/plovr/plovr_config.js
@@ -9,7 +9,7 @@
     	"checkTypes": "WARNING",
     	"checkDebuggerStatement": "WARNING"
     },
-    "inputs" : ["../../src/org/webkitjs/application/application.js", "../../sample/init.js"], // We start in application.js, but override it in init
+    "inputs" : ["../../src/org/webkitjs/application/application.js", "../../sample/js/init.js"], // We start in application.js, but override it in init
     "externs" : "jquery-1.7.js", // from http://code.google.com/p/closure-compiler/source/browse/#svn%2Ftrunk%2Fcontrib%2Fexterns
     "output-file": "../../build/app.js"
     ,"jsdoc-html-output-path": "../" // jsdoc doesnt seem to work for some reason
