23dfc25e8373bf6c5f98b5a90a2cb6824ff4a59f
niclas norgren <niclas@bindows.net>
2013-03-10 16:40:56 +0100

    correcting paths for compiler

--- tools/plovr/plovr_config.js
+++ tools/plovr/plovr_config.js
@@ -1,15 +1,15 @@
 {
 	// This file is described here: 
 	// http://plovr.com/options.html
-    "id" : "webkitjs-objects",
-    "paths" : ["../src/org/webkitjs"],
+    "id" : "webkitjs",
+    "paths" : ["../../src/org/webkitjs"],
     "mode" : "ADVANCED",
     "level" : "VERBOSE",
     "checks": { // checks are described here: http://code.google.com/p/closure-compiler/wiki/Warnings
     	"checkTypes": "WARNING",
     	"checkDebuggerStatement": "WARNING"
     },
-    "inputs" : ["./src/org/webkitjs/application.js", "../../sample/js/init.js"], // We start in application.js, but override it in init
+    "inputs" : ["../../src/org/webkitjs/application/application.js", "../../sample/init.js"], // We start in application.js, but override it in init
     "externs" : "jquery-1.7.js", // from http://code.google.com/p/closure-compiler/source/browse/#svn%2Ftrunk%2Fcontrib%2Fexterns
     "output-file": "../../build/app.js"
     ,"jsdoc-html-output-path": "../" // jsdoc doesnt seem to work for some reason
